90fa7be1a0512d275d048fc7e37b0d1395e2c11e
Thanasis17m <thanasis17m@yahoo.com>
2017-10-21 20:07:06 +0300

    Create Coordinates.java

---
+++ src/entities/Coordinates.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Coordinates {
+	Double longitude;
+	Double latitude;
+	String listing_name;
+	
+	
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public String getListing_name() {
+		return listing_name;
+	}
+	public void setListing_name(String listing_name) {
+		this.listing_name = listing_name;
+	} 
+	
+	
+}
